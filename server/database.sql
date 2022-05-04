4f601b191282df3aeb76586b46ce0d5f43dad6a2
BonsonDem <it.bonson.demssie@gmail.com>
2022-05-04 12:06:40 +0300

    backend user web api added

--- server/database.sql
+++ server/database.sql
@@ -1,12 +1,17 @@
 CREATE DATABASE testdb;
-
-CREATE TABLE users(user_id SERIAL PRIMARY KEY,
-username VARCHAR (255), password VARCHAR (255));
-
-
-
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+);
 CREATE TABLE books(
     book_id SERIAL PRIMARY KEY,
-    title VARCHAR (255),
-    author VARCHAR (255)
+    title VARCHAR NOT NULL (255),
+    author VARCHAR NOT NULL (255),
+    genre VARCHAR NOT NULL (255),
+    descr VARCHAR NOT NULL (255),
+    img VARCHAR NOT NULL (255), 
+    ISBN -13 VARCHAR NOT NULL (255),
+    price VARCHAR NOT NULL (255)
 );
--- server/index.js
+++ server/index.js
@@ -7,31 +7,76 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
+////////////////////////////////////////////////////////////////////////////////////////////
+// Users Web API
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//get all users
+app.get("/users/all", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+//create a user
+app.post("/users/create", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+//delete a user
+app.post("/users/delete", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+//edit a user
+app.post("/users/edit", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Books Web API
+////////////////////////////////////////////////////////////////////////////////////////////
+
 //create a book
-app.post("/books", async (req, res) => {
-    try {
-      const { description } = req.body;
-      const newTodo = await pool.query(
-        "INSERT INTO todo (description) VALUES($1) RETURNING *",
-        [description]
-      );
-      res.json(newTodo.rows[0]);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
+app.post("/books/create", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newBook = await pool.query(
+      "INSERT INTO books (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newBook.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a book
+app.post("/books/delete", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //get all Books
 app.get("/books", async (req, res) => {
-    try {
-      const allBooks = await pool.query("SELECT * FROM books");
-      res.json(allBooks.rows);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
+  try {
+    const allBooks = await pool.query("SELECT * FROM books");
+    res.json(allBooks.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
-    console.log("server has started on port 5000");
-  });
-  
+  console.log("server has started on port 5000");
+});
