3e246bde7fcde9ee8739be0912a0870d8e72a404
BonsonDem <it.bonson.demssie@gmail.com>
2022-05-27 09:06:00 +0300

    added dashboard, widget ,sidebar , topbar , dummydata.js, ,list of user ,product  and chart

---
+++ client/src/pages/DashboardPages/Home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import FeaturedInfo from "../../../components/DashBoard/FeaturedInfo/FeaturedInfo";
+import Chart from "../../../components/DashBoard/Chart/Chart";
+import { userData } from "../../../dummyData";
+import WidgetSm from "../../../components/DashBoard/widgetSm/WidgetSm";
+import WidgetLg from "../../../components/DashBoard/widgetLg/WidgetLg";
+import "./home.css";
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      Home
+      <Fragment>
+        <FeaturedInfo />
+        <Chart
+          data={userData}
+          title="User Analytics"
+          grid
+          dataKey="Active User"
+        />
+        <div className="homeWidgets">
+          <WidgetSm />
+          <WidgetLg />
+        </div>
+      </Fragment>
+    </div>
+  );
+}
